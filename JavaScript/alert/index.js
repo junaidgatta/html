5c219892dd878a89062306ed10aba92d75964b6d
junaidgatta <junaidgatta@yahoo.com>
2019-12-29 14:50:04 +0500

    javaScript alert+variable

---
+++ JavaScript/alert/index.js
@@ -0,0 +1,10 @@
+// alert("welcome")
+
+var user = "junaid"
+alert (user)
+
+var user = "Ahmed"
+alert (user)
+console.log(user)
+
+var quote = "your's sincerly"
---
+++ JavaScript/variables/variable.js
@@ -0,0 +1,20 @@
+var operand_one = 80
+var operand_two = 30
+
+console.log(operand_one+operand_two)
+
+var orignal_price = 1000
+var discounted_price = orignal_price-200
+console.log(discounted_price)
+
+var number_one = 1
+var number_two = 1
+var number_three = "1"
+console.log(number_three+number_one+number_two)
+
+console.log(number_one+number_two+number_three)
+
+console.log(number_three+number_one-number_two)
+
+var user ="junaid"
+console.log("welcome :"+user)
