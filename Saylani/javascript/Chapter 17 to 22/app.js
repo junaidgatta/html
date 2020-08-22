689e53a277c06cd16b95a0c221eeae3c29af0169
junaidgatta <junaidgatta@yahoo.com>
2020-08-23 02:13:54 +0500

    saylani JS chapter no.17 to 22

---
+++ Saylani/javascript/Chapter 17 to 22/app.js
@@ -0,0 +1,125 @@
+// for (var i = 0; i <= 10; i++){
+//     document.write(i+ "<br>");   
+// }
+
+// for (var i = 1; i <=10; i++){
+//     document.write("2 " + "X " + i +" = " + 2*i + "<br>")
+// }
+
+
+
+// var names= ["aziz","saleh","tariq","arif","junaid",]
+// for (var i = 0; i < names.length; i++){
+//     document.write(names[i] + "<br>");
+// }
+
+// var cities = ["Karachi","Islamabad","Lahore","Peshawar","Quetta"]
+// for (var i = 0; i < cities.length; i++){
+//     if (cities[i] === "Karachi"){
+//         document.write(cities[i] + " is the biggest city of Pakistan");
+//     }
+// }
+
+
+// var cities =["Islamabad","Lahore","Karachi","Peshawar","Quetta"]
+
+// for (var i = 0; i < cities.length; i++){
+//     if(cities[i] === "Karachi" ){
+//         document.write(cities[i] + " is the city of Pakistan")
+//     }
+// }
+
+// for (var i = 10; i > 0; i--){
+//     document.write(i + "<br>");
+// }
+
+
+// for (var i = 1; i <= 5; i++){
+//     for (var j = 5; j >= i; j--){
+//         document.write("*" + " ");
+//         console.log("*");
+//     }
+//     document.write("<br>");
+// }
+
+// for (var i = 5; i > 0; i--){
+//     for (j = 1; j < i; j++){
+//         document.write(i);
+//     }
+//     document.write("<br>");
+// }
+
+// for (var i = 1; i <100; i = i+10){
+//     for (var j = i; j < i+10; j++){
+//         document.write(j + " ");
+//     }
+//     document.write("<br>");
+// }
+
+// for(var i =1; i <=4; i++){
+//     for(var j =1;j <=i;j++){
+//         document.write("*");
+//     }
+//     document.write("<br>");
+// }
+// for (var i =1; i<=3;i++){
+//     for ( var j=3;j >=i;j--){
+//         document.write("*");
+//     }
+//     document.write("<br>");
+// }
+
+// for (var i = 1; i <=4;i++){
+//     for (var j = 4;j >=i;j--){
+//         document.write(".");
+//     }
+//     for (var k = 1;k <=i;k++){
+//         document.write("*");
+//     }
+//     document.write("<br>");
+// }
+
+// var firstName=["junaid","wahab","yaseen","shoaib"];
+// var lastName=["aziz","zahoor","yousuf","noor"];
+// var fullName=[];
+
+// for (var i=0;i <firstName.length;i++){
+//     fullName.push((firstName[i]+ " ") + lastName[i]);
+// }
+
+// Palindrome checker
+// var word=prompt("Enter your word");
+// var check="";
+
+// for (var i=word.length -1; i >=0; i--){
+//     check +=word[i];
+// }
+// if (word === check){
+//     console.log(word + " is palindrome word");
+// }
+// else {
+//     console.log(word + " is not palindrome word");
+// }
+
+// var firstName = "JUNAID"
+// firstName = firstName.toLowerCase();
+// console.log(firstName);
+// console.log(firstName.length);
+
+// var lastName=["aziz","zahoor","yousuf","noor"];
+// console.log(lastName.length);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
