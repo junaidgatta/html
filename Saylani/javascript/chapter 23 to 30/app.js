f46c0f4f60b6672b58c2398d29f26e7bb5d6110b
junaidgatta <junaidgatta@yahoo.com>
2020-08-26 01:28:55 +0500

    javascript chaper no.23 to 30

---
+++ Saylani/javascript/chapter 23 to 30/app.js
@@ -0,0 +1,92 @@
+// var srt = "to be or not to be";
+// var b = srt.replace(/be/gi, "hello");
+// console.log(b);
+
+
+// var text = 'The New Yorker magazine does not allow the phrase "World War II."';
+// var firstChar = text.indexOf("World War II.");
+// console.log(firstChar);
+
+
+// for (var i = 1;i <=100;i=i+10){
+//     for(j=i;j <i+10 ;j++){
+//     document.write(j + "  ");
+//     }
+//     document.write("<br>");
+// }
+
+// var city = prompt("Enter city name");
+// var firstChar= city.slice(0,1);
+// var otherChar= city.slice(1);
+// firstChar = firstChar.toUpperCase();
+// otherChar = otherChar.toLowerCase();
+// city = firstChar + otherChar
+// console.log(city);
+
+// var cities = ["Karachi","Islamabad","Quetta","Peshawar","Lahore"];
+// var matchfound=false;
+// for (var i = 0; i < cities.length; i++){
+//     if(cities[i] === city){
+//         (matchfound=true);
+//         alert(city + " has found");
+//     }
+//     else if(matchfound === false){
+//         alert(city + " is not in the list");
+//     }
+// }
+
+
+// var num = 1.6
+// console.log(num);
+
+// var roundNum = Math.round(num);
+// console.log(roundNum);
+
+// var num1 = 2.3;
+// var newNum1 = Math.ceil(num1);
+// console.log (newNum1);
+
+// var num2 = 4.8;
+// var  newNum2 = Math.floor(num2);
+// console.log(newNum2);
+
+
+// var headsUser = prompt("Enter Heads Username");
+// var tailsUser = prompt("Enter Tails Username");
+// var toss = Math.random() * 2;
+// var floor = Math.floor(toss);
+
+// if(floor === 0){
+//     alert("Head " + headsUser + " Win the toss");
+// }
+// else{
+//     alert("Tail " + tailsUser + " Win the toss");
+// }
+
+
+// var num = "100";
+// console.log(num);
+
+// var StrToInt = Number(num);
+// console.log(StrToInt);
+
+// console.log(StrToInt.toString());
+
+// num = Number(num);
+// console.log(num);
+
+
+var num3 = 11.563846
+var contDec = num3.toFixed(2);
+console.log(contDec);
+
+
+
+
+
+
+
+
+
+
+
