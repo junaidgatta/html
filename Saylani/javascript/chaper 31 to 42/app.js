d9d0b910e490772a48893485e492019566e27137
junaidgatta <junaidgatta@yahoo.com>
2020-09-03 02:04:34 +0500

    saylani javascript chapter no.31 to 42

---
+++ Saylani/javascript/chaper 31 to 42/app.js
@@ -0,0 +1,133 @@
+// var today = new Date();
+
+// console.log(today);
+
+// b=today.toString();
+// console.log(b);
+
+// var day = today.getDay();
+// console.log(day);
+
+// var todayMili = today.getTime();
+// console.log(todayMili);
+
+
+// age calculator from dob
+// var dobPrompt = prompt("Enter your date of birth","Jan 01, 1999");
+// var dob = new Date (dobPrompt);
+// // var dob = new Date ("september 05,1987");
+// console.log (dob);
+
+// var dobMili = dob.getTime();
+// console.log(dobMili);
+
+// var diff = todayMili - dobMili;
+// console.log(diff);
+
+// var dobYear = diff / (1000 * 60 * 60 * 24 * 30 *12);
+// console.log(Math.floor(dobYear));
+// alert("Your age is " + Math.floor(dobYear) + " Years");
+
+// function add(a,b,c){
+//     return a+b+c
+// }
+
+// var a = add(6,5,9);
+// console.log(a);
+
+// function tellTime() {
+//     var now = new Date();
+//     var hour = now.getHours();
+//     var min = now.getMinutes();
+//     var sec = now.getSeconds();
+//     alert("Current Time: " + hour + ":" + min + ":" + sec);
+// }
+
+// tellTime();
+
+
+// switch statement
+// var name = "Ahmed"
+
+// switch (name){
+//     case "Junaid":
+//         alert("Hello Junaid")
+//         break;
+//     case "Ahmed":
+//         alert("Hello Ahmed")
+//         break;
+//     default:
+//         alert("Hello")
+// }
+
+// var a = prompt("Please enter charachter");
+
+// switch (a){
+//     case "A":
+//     case "E":
+//     case "I":
+//     case "O":
+//     case "U":
+//         console.log(a + " is vowel");
+//         break;
+//     default:
+//         console.log(a + " is not vowel");
+// }
+
+
+
+// // while loop
+// var i = 0;
+
+// while (i<10) {
+//     document.write(i + "<br>");
+//     i++
+// }
+
+// var doContinue = true;
+
+// while (doContinue) {
+//     console.log("Hello");
+
+//     var userInput = prompt("Please enter Y to run again or any key to stop");
+//     if(userInput.toLowerCase() !== "y"){
+//         doContinue = false;
+//     }
+// }
+// console.log("Loop End");
+
+// while loop nasted
+// var height = 2;
+// var width = 8;
+
+// var col = 0;
+// var row = 0;
+// document.write("Starting Loop<br> ");
+
+// while (row < height) {
+//     col = 0;
+
+//     while (col < width) {
+//         document.write("#");
+//         col++;
+//     }
+//     document.write("<br>");
+//     row++;
+// }
+// document.write("Loop stopped!");
+
+// do while loop
+// var a = 0;
+// do{
+//     console.log(a);
+//     a++
+// }
+// while(a < 5);
+
+
+
+
+
+
+
+
