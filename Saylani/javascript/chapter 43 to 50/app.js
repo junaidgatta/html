f1c51aa19e017d0f5d3b2a5eefe59875eee4068a
junaidgatta <junaidgatta@yahoo.com>
2020-09-06 02:58:26 +0500

    saylani javascript chapter no.43 to 50

---
+++ Saylani/javascript/chapter 43 to 50/app.js
@@ -0,0 +1,18 @@
+// function hello(){
+//     document.write("Hello Everyone");
+// }
+
+// function getName(){
+//     var name = document.getElementById("name");
+//     alert(name.value);
+//     console.log(name.value);
+// }
+
+function setName (){
+    var name = document.getElementById("name");
+    name.value="Junaid"
+}
+setName();
+
+var para = document.getElementById("para");
+para.innerHTML= "Hello! this is example"
