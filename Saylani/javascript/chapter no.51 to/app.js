d49e94da3851a9fe1fb7149fe56b4c6280c7b54a
junaidgatta <junaidgatta@yahoo.com>
2020-09-25 00:40:15 +0500

    saylani javascript class

---
+++ Saylani/javascript/chapter no.51 to/app.js
@@ -0,0 +1,43 @@
+// function readMore(){
+//     var text = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi exercitationem fugiat odit non illo consequuntur tenetur tempora fuga esse repudiandae. Rem, quia perferendis ad iste possimus sequi hic expedita laboriosam";
+//     var paragraph = document.getElementById("para");
+//     paragraph.innerHTML = text;
+//     console.log(text);
+// }
+
+// function readMore(){
+//     var text = document.getElementById("text").innerHTML;
+//     var para = document.getElementById("para");
+//     para.innerHTML=text;
+// }
+
+// function changeImg(){
+//     var img = document.getElementById('img1');
+//     img.src = 'IMG2.jpg'
+// }
+
+// function changeImg1(){
+//     var img = document.getElementById('img1');
+//     img.src = 'IMG1.jpg'
+// }
+
+// function changeImg(id,ref){
+//     var image = document.getElementById(id);
+//     image.src = ref;
+
+// }
+
+var para = document.getElementsByTagName('p');
+for (let index = 0; index < para.length; index++) {
+    para[index].style.color = 'blue';
+    
+}
+
+
+
+
+
+
+
+
+
