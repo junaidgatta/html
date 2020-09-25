11dc9386f906030b8b36719720cf31f20546104a
junaidgatta <junaidgatta@yahoo.com>
2020-09-26 03:15:48 +0500

    Stopwatch

---
+++ Stopwatch JS/app.js
@@ -0,0 +1,38 @@
+var msec = 0;
+var sec = 0;
+var min = 0;
+var msecHeading = document.getElementById('mSec');
+var secHeading = document.getElementById('sec');
+var minHeading = document.getElementById('min');
+var interval;
+
+function timer(){
+    msec++
+    msecHeading.innerHTML = msec;
+    if (msec >= 100){
+        sec++
+        secHeading.innerHTML = sec;
+        msec = 0;
+    }else if (sec >= 60){
+        min++
+        minHeading.innerHTML = min;
+        sec=0;
+    }
+}
+
+
+function start(){
+    interval = setInterval (timer,10);
+    document.getElementById('start').disabled = true;
+}
+
+function stop(){
+    clearInterval(interval);
+    document.getElementById('start').disabled = false;
+}
+
+function reset(){
+    msecHeading.innerHTML = 0;
+    secHeading.innerHTML = 0;
+    minHeading.innerHTML = 0;
+}
